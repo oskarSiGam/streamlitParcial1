ebb65c484dc2525947fc1f7b81c059abd5e149bc
oskarSiGam <114121784+oskarSiGam@users.noreply.github.com>
2022-09-21 20:48:12 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+
+dfCalificaciones = pd.read_excel("tareas.xlsx")
+
+dfCalificaciones["promedio"]= (dfCalificaciones["tarea1"]+dfCalificaciones["tarea2"]+dfCalificaciones["tarea3"]+dfCalificaciones["tarea4"])/4
+
+dfCalificaciones["status"]=dfCalificaciones["promedio"].apply(lambda x: "APROBADO" if (x >= 70)else "REPROBADO")
+
+dfResultados = dfCalificaciones[["matricula","promedio","status"]]
+
+dfResultados
+
+st.dataframe(dfResultados)
